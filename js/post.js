71d8072f571b1dfd84e8a9db5039c64f735633d5
wli12 <liweithu@liweithu-macbookair.roam.corp.google.com>
2014-09-14 15:59:40 +0800

    modify post.js

--- js/post.js
+++ js/post.js
@@ -93,7 +93,7 @@ $(document).ready(function(){
             var tmpl = genTmpl();
             var indexCon = '<div id="menuIndex" class="sidenav"></div>';
 
-            $('#content').insert(1, indexCon);
+            $('#weibo').after(indexCon);
 
             $('#menuIndex')
                 .append($(tmpl))
